d293115f4ad41792aac94d316eb10d63576118bf
AlexS778 <aleksej.serbinov29@gmail.com>
2022-02-13 16:34:55 +0000

    first working version

--- bot_V1.0.py
+++ bot_V1.0.py
@@ -1,36 +1,54 @@
 import time
+
 import discord
 import requests
+import schedule
 from bs4 import BeautifulSoup
 
 client = discord.Client()
 
 
-def write_to_file():
-    r = requests.get("https://forums.eveonline.com/c/marketplace/sales-ads/")
-    stuff_list = []
+def get_info(forum_url, list_name):
+    r = requests.get(forum_url)
     soup = BeautifulSoup(r.text, "lxml")
-    tags = soup("a", class_="title raw-link raw-topic-link")
+    tags = soup.find_all("a", class_="title raw-link raw-topic-link")
     for tag in tags:
         item_url = tag.get("href")
         item_text = tag.text
-        stuff_list.append(item_text)
-        stuff_list.append(item_url)
-    return stuff_list
+        list_name.append(item_text)
+        list_name.append(item_url)
+    return list_name
 
 
-data = write_to_file()
-# for line in data:
-# print(line.strip())
+def compare(forum_url):
+    state1_list, state2_list, final_list = [], [], []
+    list1 = get_info(forum_url, state1_list)
+    time.sleep(120)
+    list2 = get_info(forum_url, state2_list)
+
+    if list1[2] != list2[2]:
+        final1 = list2[2:4].copy()
+        # print(list2[2], "\n ")
+        # print(list2[3], "\n ")
+        return final1
+    state1_list.clear()
+    state2_list.clear()
+
+
+def compare_on_time(forum_url):
+    schedule.every(1).seconds.do(compare, forum_url)
+    while True:
+        schedule.run_pending()
+
+
+data = compare("https://forums.eveonline.com/c/marketplace/sales-ads/")
 
 
 @client.event
 async def on_message(message):
-    if message.content.startswith(
-        "!start"
-    ):  # the command you want to use e.g if you want to use .ff DRSP set to .ff
+    if message.content.startswith("!start"):
         for line in data:
-            await message.channel.send(line.strip())
+            await message.channel.send(line)
     elif message.content.startswith("hello"):
         await message.channel.send("hi,i am a bot")
     else:
