549edd73cec08323cf6abd523ddc8ad22d29f57e
AlexS778 <aleksej.serbinov29@gmail.com>
2022-03-08 14:58:49 +0300

    Co-authored-by: Kidein <Kidein@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -14,7 +14,14 @@ async def on_message(message):
             for line in data:
                 await message.channel.send(line)
             data.clear()
+    if message.content.startswith("!start_new_topics"):
+        while True:
+            data = main.compare_for_new_topics(
+                "https://forums.eveonline.com/c/marketplace/sales-ads/"
+            )
+            for line in data:
+                await message.channel.send(line)
+            data.clear()
 
 
 client.run(BOT_TOKEN)
-
--- main.py
+++ main.py
@@ -5,24 +5,47 @@ import schedule
 from bs4 import BeautifulSoup
 
 
-def get_info(forum_url, list_name):
+def get_link(forum_url, links_list):
     r = requests.get(forum_url)
     soup = BeautifulSoup(r.text, "lxml")
     tags = soup.find_all("a", class_="title raw-link raw-topic-link")
     for tag in tags:
-        item_url = tag.get("href")
-        list_name.append(item_url)
-    return list_name
+        url = tag.get("href")
+        links_list.append(url)
+    return links_list
+
+
+def get_replies(forum_url, replies_list):
+    r = requests.get(forum_url)
+    soup = BeautifulSoup(r.text, "lxml")
+    replies = soup.find_all("span", class_="posts")
+    for reply in replies:
+        replies_list.append(reply.contents)
 
 
 def compare(forum_url):
-    state1_list, state2_list = [], []
-    list1 = get_info(forum_url, state1_list)
+    links_list1, links_list2 = [], []
+    state1_list = get_link(forum_url, links_list1)
     time.sleep(60)
-    list2 = get_info(forum_url, state2_list)
+    state2_list = get_link(forum_url, links_list2)
 
-    if list1[1] != list2[1]:
-        final1 = list2[1:2].copy()
-        return final1
+    if state1_list[1] != state2_list[1]:
+        return state2_list[1:2]
+    else:
+        return []
+
+
+def compare_for_new_topics(forum_url):
+    replies_list1, links_list01, links_list02 = [], [], []
+    first_state_list = get_link(forum_url, links_list01)
+    time.sleep(60)
+    second_state_list = get_link(forum_url, links_list02)
+    replies_list = get_replies(forum_url, replies_list1)
+    x = replies_list[1]
+    if x[0] == "0":
+        if first_state_list[1] != second_state_list[1]:
+            return second_state_list[1:2]
+        else:
+            return []
     else:
         return []
